cb1696c7a55c9381d216745840537a9a7547de1b
Gareth Scott-Dodd <garethscott-dodd@gareths-mbp.home>
2021-11-25 10:31:55 +0000

    ssh directions

--- ec2.tf
+++ ec2.tf
@@ -12,10 +12,25 @@ resource "aws_security_group" "my_app_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["94.11.95.130/32"]
   }
+   ingress {
+    description      = "SSH from my VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["192.168.0.0/16"]
+  }
 
   tags = {
       Name = "my_app_sg"
   }
+
+  egress {
+  description = "Allow access to the world"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 data "aws_ami" "my_aws_ami" {
@@ -26,22 +41,22 @@ data "aws_ami" "my_aws_ami" {
         values = [ "amzn2-ami-kernel-*" ]
     }
 }
-
+#EC2- PRIVATE
 resource "aws_instance" "my_first_server" {
     ami = data.aws_ami.my_aws_ami.id
     instance_type = var.instance_type
     key_name = var.keypair_name 
     subnet_id = aws_subnet.private_a.id
-    security_groups = [aws_security_group.my_app_sg.id]
+    vpc_security_group_ids = [aws_security_group.my_app_sg.id]
 
 }
-
+#EC2- PUBLIC
 resource "aws_instance" "public_server_a" {
   ami = data.aws_ami.my_aws_ami.id
   instance_type = var.instance_type
   key_name = var.keypair_name
   subnet_id = aws_subnet.public_a.id
-  security_groups = [aws_security_group.my_app_sg.id]
+  vpc_security_group_ids = [aws_security_group.my_app_sg.id]
 }
 
 #AMI ID
@@ -50,3 +65,7 @@ resource "aws_instance" "public_server_a" {
 #SUBNET- PRIVATE
 #SECURITY GROUP
 #USER-DATA TEMPLATE
+#SSH COMMAND
+#ssh -i ~/.ssh/<KEY NAME>.pem ec2-user@<IP ADDRESS>
+#COPY PRIVATE KEY TO PUBLIC SERVER
+#scp -i ~/.ssh/ta-lab-key.pem ~/.ssh/ta-lab-key.pem ec2-user@<PUBLICSERVER>:/home/ec2-user/
